b84e1050444478ddf7ea9aa4800f3b1cfbd2f993
styunlen <styunlen@qq.com>
2022-12-08 20:31:09 +0800

    update readme

--- src/config/database.ts
+++ src/config/database.ts
@@ -8,66 +8,15 @@ Logger.info(`sqlFilePath : ${path.join(__dirname, "./../data/database.db")}`);
 Logger.info(
   `entitiesFile Path:${path.join(__dirname + "./../models/*{.js,.ts}")}`
 );
-// { BUG FOUND
-//   const arr = [];
-//   // indicates if a dir is the last item of fileList readdir returned
-//   let isLastDir = true;
-
-//   const fileDisplay = (filePath: string) => {
-//     return new Promise((resolve, reject) => {
-//       fs.readdir(filePath, function (err, files) {
-//         if (err) {
-//           console.error("ReadDir Error:");
-//           reject(err);
-//         }
-//         files.forEach((filename, fileIndex, fileArr) => {
-//           const filedir = path.join(filePath, filename);
-//           fs.stat(filedir, (error, stats) => {
-//             if (error) {
-//               console.error("Read FileStat Error:");
-//               reject(error);
-//             }
-//             const isFile = stats.isFile();
-//             const isDir = stats.isDirectory();
-//             console.log(fileIndex, filedir);
-//             if (isFile) {
-//               arr.push(filedir);
-//             }
-//             if (isDir) {
-//               isLastDir = fileIndex == fileArr.length - 1;
-//               return fileDisplay(filedir).then(arr => {
-//                 if (fileIndex == fileArr.length - 1) {
-//                   console.log(fileIndex);
-//                   resolve(arr);
-//                 }
-//               });
-//             }
-//             /* Only if fileList contains dir
-//              and the dir is the last item of the list(isLastDir == true)
-//              promise can resolve
-//           */
-//             if (fileIndex == fileArr.length - 1 && isLastDir) {
-//               resolve(arr);
-//             }
-//           });
-//         });
-//       });
-//     });
-//   };
-//   fileDisplay(path.join(__dirname + "./../"))
-//     .then(arr => {
-//       console.log(arr);
-//     })
-//     .catch(err => console.log(err));
-// }
-// const entities = fileDisplaySync(path.join(__dirname + "./../"));
-// console.log(entities);
-
-// Typeorm's entities loader seems not work
-// This two functions below is example to list models files
 fileDisplay(path.join(__dirname + "./../models/")).then(arr =>
   console.log(arr)
 );
+Logger.info(
+  `migration Path:${path.join(__dirname + "./../migrations/*{.js,.ts}")}`
+);
+fileDisplay(path.join(__dirname + "./../migrations/")).then(arr => {
+  console.log(arr);
+});
 fileDisplaySync(path.join(__dirname + "./../models/"));
 
 export const getDatabaseType = () => {
